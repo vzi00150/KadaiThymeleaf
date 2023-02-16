49c025aa9c96ebe204d0968f430d35916afc35ae
vzi00150 <vzi00150@gmail.com>
2023-02-16 21:42:09 +0900

    課題提出としては一回目

--- src/main/java/com/techacademy/InputController.java
+++ src/main/java/com/techacademy/InputController.java
@@ -3,23 +3,17 @@ package com.techacademy;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class InputController {
 
-    @GetMapping("/input/{previous}")
-    public String getInput(@PathVariable("previous") String previous, Model model) {
+    @GetMapping("/input")
+    public String getInput(@RequestParam(name = "previous", required = false) String previous, Model model) {
+        //パラメータをmodelオブジェクトに設定
         model.addAttribute("previous", previous);
         // input.htmlに画面遷移
         return "input";
     }
 
-    /*
-    @GetMapping("/input")
-    public String getInput() {
-        // input.htmlに画面遷移
-        return "input";
-    }
-    */
 }
